ea4fd3c1835821136cc8648926cc78ef468a03b0
Roland Sobczak <rolandsobczak@icloud.com>
2023-10-03 15:57:45 +0200

    feat: get_all_nested_files helper

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,4 +1,6 @@
 import json
+import os
+from pathlib import Path
 
 from backend.db import SessionLocal
 from backend.parser import parse_receipt, parse_receipt_item
@@ -7,7 +9,17 @@ from backend.repositories import create_receipt, create_receipts_item
 JSON_FILENAME = "07.json"
 
 
+def get_all_nested_files(root_dir: Path):
+    file_paths = []
+    for root, directories, files in os.walk(root_dir):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            file_paths.append(file_path)
+    print(file_paths)
+
+
 def main():
+    # print(get_all_nested_files(Path("/input")))
     db = SessionLocal()
     with open(f"/input/{JSON_FILENAME}", encoding="utf-8") as input:
         receipt_json = json.loads(input.read())
